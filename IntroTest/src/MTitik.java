36d0e2624fc1a58d5bebb9d03feadd27565eec1d
diahmaulida555 <diahmaulida555@gmail.com>
2026-02-26 09:26:04 +0700

    hasil dari praktikum pertemuan 2

--- IntroTest/src/MTitik.java
+++ IntroTest/src/MTitik.java
@@ -1,7 +1,7 @@
 /**
  * Nama File    : MTitik.java
  * Deskripsi    : main class untuk mencoba class Titik
- * Pembuat      : diah maulida pratiwi
+ * Pembuat      : Diah maulida pratiwi
  * Tanggal      : 19 Februari 2026
  */
 
@@ -17,9 +17,11 @@ public class MTitik {
         T1.setOrdinat(4); 
         
         // Mencetak koordinat T1 ke layar
+        System.out.println("Koordinat T1: ");
         T1.printTitik(); 
         
         // Menggeser T1 sejauh (3,4)
+        System.out.println("Menggeser T1 sejauh (3,4)");
         T1.geser(3, 4); 
         
         // Menampilkan koordinat T1 setelah digeser
@@ -28,20 +30,28 @@ public class MTitik {
         // Menampilkan kuadran T1 setelah digeser
         System.out.println("Kuadran T1: " + T1.getKuadran());
 
-        // Menampilkan jumlah objek Titik yang telah dibuat
-        System.out.println("Jumlah objek Titik yang telah dibuat: " + Titik.getCounterTitik());
 
         // setRefleksiX untuk T1
+        System.out.println("Refleksi terhadap sumbu X untuk T1: ");
         T1.setRefleksiX();
 
         // Menampilkan koordinat T1 setelah direfleksikan terhadap sumbu X
         T1.printTitik();
 
         // setRefleksiY untuk T1
+        System.out.println("Refleksi terhadap sumbu Y untuk T1: ");
         T1.setRefleksiY();
 
         // Menampilkan koordinat T1 setelah direfleksikan terhadap sumbu Y
         T1.printTitik();
+
+        // menampilkan getsRefleksiX untuk T1
+        System.out.println("Refleksi objek baru sumbu X T1:");
+        T1.getRefleksiX().printTitik();
+
+        // menampilkan getsRefleksiY untuk T1
+        System.out.println("Refleksi objek baru sumbu Y T1:");
+        T1.getRefleksiY().printTitik();
 
         // Jarak untuk T1 ke titik lain (5,6)
         Titik T_lain = new Titik();
@@ -54,19 +64,24 @@ public class MTitik {
         double jarakPusat = T1.getJarakPusat();
         System.out.println("Jarak T1 ke titik pusat (0,0): " + jarakPusat);
 
+        // Menampilkan jumlah objek Titik yang telah dibuat
+        System.out.println("Jumlah objek Titik yang telah dibuat: " + Titik.getCounterTitik());
+
         // Membuat objek titik T1 (0,0)
         Titik T2 = new Titik(); 
         
-        // Mengubah absis T2 dengan nilai 3
-        T2.setAbsis(3); 
+        // Mengubah absis T2 dengan nilai 4
+        T2.setAbsis(4); 
         
         // Mengubah ordinat T2 dengan nilai 4
         T2.setOrdinat(4); 
         
         // Mencetak koordinat T2 ke layar
+        System.out.println("Koordinat T2: ");
         T2.printTitik(); 
         
         // Menggeser T2 sejauh (3,4)
+        System.out.println("Menggeser T2 sejauh (3,4)");
         T2.geser(3, 4); 
         
         // Menampilkan koordinat T2 setelah digeser
@@ -75,16 +90,15 @@ public class MTitik {
         // Menampilkan kuadran T2 setelah digeser
         System.out.println("Kuadran T2: " + T2.getKuadran());
 
-        // Menampilkan jumlah objek Titik yang telah dibuat
-        System.out.println("Jumlah objek Titik yang telah dibuat: " + Titik.getCounterTitik());
-
         // setRefleksiX untuk T2
+        System.out.println("Refleksi terhadap sumbu X untuk T2: ");
         T2.setRefleksiX();
 
         // Menampilkan koordinat T2 setelah direfleksikan terhadap sumbu X
         T2.printTitik();
 
         // setRefleksiY untuk T2
+        System.out.println("Refleksi terhadap sumbu Y untuk T2: ");
         T2.setRefleksiY();
 
         // Menampilkan koordinat T2 setelah direfleksikan terhadap sumbu Y
@@ -100,7 +114,16 @@ public class MTitik {
         // Menampilkan jarakpusat T2 ke titik pusat (0,0)
         double jarakPusat2 = T2.getJarakPusat();
         System.out.println("Jarak T2 ke titik pusat (0,0): " + jarakPusat2);
-        
+
+        // mengembalikan getsRefleksiX untuk T2
+        System.out.println("Refleksi objek baru sumbu X T2:");
+        T2.getRefleksiX().printTitik();
+
+        // mengembalikan getsRefleksiY untuk T2
+        System.out.println("Refleksi objek baru sumbu Y T2:");
+        T2.getRefleksiY().printTitik();
+
+    
         // menghitung jumlah objek Titik yang telah dibuat
         System.out.println("Jumlah objek Titik yang telah dibuat: " + Titik.getCounterTitik());
     }
