4419d49784cddd55bb95fc52f9c0215959a6c8b3
diahmaulida555 <diahmaulida555@gmail.com>
2026-02-26 09:46:57 +0700

    method tambahan dari pertemuan2 di lab

---
+++ IntroTest/src/Garis.java
@@ -0,0 +1,17 @@
+/*
+Nama File  : Garis.java
+Deskripsi  : kelas yang berisi atribut dan method untuk membuat objek garis
+Pembuat    : Diah Maulida Pratiwi
+Tanggal    : 26 Februari 2026
+*/
+
+public class Garis {
+    /*************************** Atribut ***************************/
+    Titik titik1;
+    Titik titik2;
+    static int counterGaris = 0; // atribut untuk menghitung jumlah objek Garis yang dibuat
+
+    /*************************** Konstruktor ***************************/
+    
+
+}
