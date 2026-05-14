88df8384180eeef1b99d445c19df2e0350286deb
diahmaulida555 <diahmaulida555@gmail.com>
2026-05-14 08:15:26 +0700

    PBO08 - hanya folder PBO08_24060124120034

--- Pertemuan5/src/Pajak.java
+++
@@ -1,8 +0,0 @@
-//Nama File = Pajak.java
-//Deskripsi = membuat interface pajak
-//Pembuat   = Diah Maulida Pratiwi-24060124120034
-//Tanggal   = 20 Maret 2026
-public interface Pajak {
-    //method menghitung pajak 
-    public double hitungPajak();
-}
--- Pertemuan6/src/AngkaSialException.java
+++
@@ -1,9 +0,0 @@
-/**
- * File      : AngkaSialException.java
- * Deskripsi : Eksepsi buatan sendiri, menolak masukan angka 13!
- */
-public class AngkaSialException extends Exception {
-    public AngkaSialException() {
-        super("jangan masukkan angka 13 karena angka sial !!!");
-    }
-}
